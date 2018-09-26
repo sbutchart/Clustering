982d064c1fbd1d2fca08de26f8e28aa293f15d18
plasorak <pierre.lasorak@gmail.com>
2018-09-26 17:30:58 -0500

    Maginal

--- clustering/BiTrigger.cc
+++ clustering/BiTrigger.cc
@@ -58,7 +58,6 @@ void BiTrigger::AddEventToStudy(const std::vector<std::unique_ptr<WireCluster>>&
     for (auto const& it_sign: it.second)
       fSignCluster.push_back(it_sign);
   }
-  
   for (auto const& it: apa_cluster_back) {
     for (auto const& it_back: it.second)
       fBackCluster.push_back(it_back);
@@ -70,23 +69,10 @@ void BiTrigger::FinishStudy() {
   fPt_nClu_Back = new TGraph(fBackCluster.size());
   fPt_Peak_Sign = new TGraph(fSignCluster.size());
   fPt_Peak_Back = new TGraph(fBackCluster.size());
-  fPt_nHit_Sign = new TGraph(fSignCluster.size());
-  fPt_nHit_Back = new TGraph(fBackCluster.size());
-
-  
-  fLikelihood_nClu_Sign = new TH2D("Likelihood_nClu_Sign", "",   10, -0.5,   9.5,  10, -0.5,   9.5);
-  fLikelihood_nClu_Back = new TH2D("Likelihood_nClu_Back", "",   10, -0.5,   9.5,  10, -0.5,   9.5);
-  fLikelihood_Peak_Sign = new TH2D("Likelihood_Peak_Sign", "", 1000, -0.5, 999.5, 300, -0.5, 299.5);
-  fLikelihood_Peak_Back = new TH2D("Likelihood_Peak_Back", "", 1000, -0.5, 999.5, 300, -0.5, 299.5);
-  fLikelihood_nHit_Sign = new TH2D("Likelihood_nHit_Sign", "",  100, -0.5,  99  , 100, -0.5,  99  );
-  fLikelihood_nHit_Back = new TH2D("Likelihood_nHit_Back", "",  100, -0.5,  99  , 100, -0.5,  99  );
-
-  fLikelihood_nClu_Sign->SetStats(0);
-  fLikelihood_nClu_Back->SetStats(0);
-  fLikelihood_Peak_Sign->SetStats(0);
-  fLikelihood_Peak_Back->SetStats(0);
-  fLikelihood_nHit_Sign->SetStats(0);
-  fLikelihood_nHit_Back->SetStats(0);
+  fLikelihood_nClu_Sign = new TH2D("Likelihood_nClu_Sign", "", 10, 0, 10,  10, 0, 10);
+  fLikelihood_nClu_Back = new TH2D("Likelihood_nClu_Back", "", 10, 0, 10,  10, 0, 10);
+  fLikelihood_Peak_Sign = new TH2D("Likelihood_Peak_Sign", "", 100, 0, 1000, 100, 0, 1000);
+  fLikelihood_Peak_Back = new TH2D("Likelihood_Peak_Back", "", 100, 0, 1000, 100, 0, 1000);
 
   
   int i=0;
@@ -119,52 +105,34 @@ void BiTrigger::FinishStudy() {
   fPt_nHit_Back->SetMarkerColor(kBlue);
   
   TCanvas c;
-  gPad->SetLogz();
-    
   fPt_nClu_Sign->SetMarkerSize(2);
   fPt_nClu_Back->SetMarkerSize(2);
   fPt_Peak_Sign->SetMarkerSize(2);
   fPt_Peak_Back->SetMarkerSize(2);
-  fPt_nHit_Sign->SetMarkerSize(2);
-  fPt_nHit_Back->SetMarkerSize(2);
-  
   fPt_nClu_Sign->SetTitle(""); fPt_nClu_Sign->SetName("nClu_Sign");
   fPt_nClu_Back->SetTitle(""); fPt_nClu_Back->SetName("nClu_Back");
   fPt_Peak_Sign->SetTitle(""); fPt_Peak_Sign->SetName("Peak_Sign");
   fPt_Peak_Back->SetTitle(""); fPt_Peak_Back->SetName("Peak_Back");
-  fPt_nHit_Sign->SetTitle(""); fPt_nHit_Sign->SetName("nHit_Sign");
-  fPt_nHit_Back->SetTitle(""); fPt_nHit_Back->SetName("nHit_Back");
 
-  fPt_nClu_Sign->GetXaxis()->SetTitle("number of wire clusters / 1#times2#times6");
-  fPt_nClu_Back->GetXaxis()->SetTitle("number of wire clusters / 1#times2#times6");
-  fPt_nClu_Sign->GetYaxis()->SetTitle("number of optical clusters / 1#times2#times6");
-  fPt_nClu_Back->GetYaxis()->SetTitle("number of optical clusters / 1#times2#times6");
+  fPt_nClu_Sign->GetXaxis()->SetTitle("number of wire clusters / APA");
+  fPt_nClu_Back->GetXaxis()->SetTitle("number of wire clusters / APA");
+  fPt_nClu_Sign->GetYaxis()->SetTitle("number of optical clusters / APA");
+  fPt_nClu_Back->GetYaxis()->SetTitle("number of optical clusters / APA");
 
-  fPt_Peak_Sign->GetXaxis()->SetTitle("Sum ADC wire cluster / 1#times2#times6");
-  fPt_Peak_Back->GetXaxis()->SetTitle("Sum ADC wire cluster / 1#times2#times6");
-  fPt_Peak_Sign->GetYaxis()->SetTitle("Sum PE optical cluster / 1#times2#times6");
-  fPt_Peak_Back->GetYaxis()->SetTitle("Sum PE optical cluster / 1#times2#times6");
+  fPt_Peak_Sign->GetXaxis()->SetTitle("Sum ADC wire cluster / APA");
+  fPt_Peak_Back->GetXaxis()->SetTitle("Sum ADC wire cluster / APA");
+  fPt_Peak_Sign->GetYaxis()->SetTitle("Sum PE optical cluster / APA");
+  fPt_Peak_Back->GetYaxis()->SetTitle("Sum PE optical cluster / APA");
 
-  fPt_nHit_Sign->GetXaxis()->SetTitle("number of hits in wire clusters / 1#times2#times6");
-  fPt_nHit_Back->GetXaxis()->SetTitle("number of hits in wire clusters / 1#times2#times6");
-  fPt_nHit_Sign->GetYaxis()->SetTitle("number of hits in optical clusters / 1#times2#times6");
-  fPt_nHit_Back->GetYaxis()->SetTitle("number of hits in optical clusters / 1#times2#times6");
 
-  fLikelihood_nClu_Sign->GetXaxis()->SetTitle("number of wire clusters / 1#times2#times6");
-  fLikelihood_nClu_Back->GetXaxis()->SetTitle("number of wire clusters / 1#times2#times6");
-  fLikelihood_nClu_Sign->GetYaxis()->SetTitle("number of optical clusters / 1#times2#times6");
-  fLikelihood_nClu_Back->GetYaxis()->SetTitle("number of optical clusters / 1#times2#times6");
-
-  fLikelihood_Peak_Sign->GetXaxis()->SetTitle("Sum ADC wire cluster / 1#times2#times6");
-  fLikelihood_Peak_Back->GetXaxis()->SetTitle("Sum ADC wire cluster / 1#times2#times6");
-  fLikelihood_Peak_Sign->GetYaxis()->SetTitle("Sum PE optical cluster / 1#times2#times6");
-  fLikelihood_Peak_Back->GetYaxis()->SetTitle("Sum PE optical cluster / 1#times2#times6");
-
-  fLikelihood_nHit_Sign->GetXaxis()->SetTitle("number of hits in wire clusters / 1#times2#times6");
-  fLikelihood_nHit_Back->GetXaxis()->SetTitle("number of hits in wire clusters / 1#times2#times6");
-  fLikelihood_nHit_Sign->GetYaxis()->SetTitle("number of hits in optical clusters / 1#times2#times6");
-  fLikelihood_nHit_Back->GetYaxis()->SetTitle("number of hits in optical clusters / 1#times2#times6");
-
+  fLikelihood_nClu_Sign->GetXaxis()->SetTitle("number of wire clusters / APA");
+  fLikelihood_nClu_Back->GetXaxis()->SetTitle("number of wire clusters / APA");
+  fLikelihood_nClu_Sign->GetYaxis()->SetTitle("number of optical clusters / APA");
+  fLikelihood_nClu_Back->GetYaxis()->SetTitle("number of optical clusters / APA");
+  fLikelihood_Peak_Sign->GetXaxis()->SetTitle("Sum ADC wire cluster / APA");
+  fLikelihood_Peak_Back->GetXaxis()->SetTitle("Sum ADC wire cluster / APA");
+  fLikelihood_Peak_Sign->GetYaxis()->SetTitle("Sum PE optical cluster / APA");
+  fLikelihood_Peak_Back->GetYaxis()->SetTitle("Sum PE optical cluster / APA");
   fPt_nClu_Sign->Draw("AP");
   fPt_nClu_Back->Draw("P");
   c.SaveAs("BiTrigger0.png");
@@ -173,20 +141,16 @@ void BiTrigger::FinishStudy() {
   fPt_Peak_Back->Draw("P");
   c.SaveAs("BiTrigger1.png");
 
-  fPt_nHit_Sign->Draw("AP");
-  fPt_nHit_Back->Draw("P");
+  fLikelihood_nClu_Sign->Draw("COLZ");
   c.SaveAs("BiTrigger2.png");
 
-  c.Print("BiTrigger.pdf[");
-  fLikelihood_nClu_Sign->Draw("COLZ");
-  c.Print("BiTrigger.pdf");
   fLikelihood_nClu_Back->Draw("COLZ");
-  c.Print("BiTrigger.pdf");
+  c.SaveAs("BiTrigger3.png");
 
   fLikelihood_Peak_Sign->Draw("COLZ");
-  c.Print("BiTrigger.pdf");
+  c.SaveAs("BiTrigger4.png");
   fLikelihood_Peak_Back->Draw("COLZ");
-  c.Print("BiTrigger.pdf");
+  c.SaveAs("BiTrigger5.png");
   
   fLikelihood_nHit_Back->Draw("COLZ");
   c.Print("BiTrigger.pdf");
