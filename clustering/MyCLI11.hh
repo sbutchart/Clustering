5713153ffd4a014eb7bcf2fb476a67fa2deb5043
Pierre Lasorak <plasorak@fnal.com>
2019-04-29 11:57:27 -0500

    Get this to work on linux (god I'm starting to hate A**LE)

--- clustering/MyCLI11.hh
+++ clustering/MyCLI11.hh
@@ -1,4 +1,8 @@
+#if defined (__APPLE__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic warning "-W#warnings"
+#endif
 #include "CLI11.hpp"
+#if defined (__APPLE__)
 #pragma GCC diagnostic pop
+#endif
