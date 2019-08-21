948f57f0f03e48cfe1407b0d9917ee42fc053c1b
plasorak <pierre.lasorak@gmail.com>
2019-08-21 11:09:34 +0100

    change the name of the plotting script. Make it work. Get rid of the c++ file.

--- TriggerDecision/app/MakePlots.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot as plt
-import uproot
-
-file =uproot.open('LMC.root')
-tree = file['Throws']
-back = tree.array('BackgroundStatistic')
-_ = plt.hist(back, bins=100)
-
-
