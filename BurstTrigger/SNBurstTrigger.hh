5f6d306f30548dab5416e2d476eb4b622e1fa552
plasorak <pierre.lasorak@gmail.com>
2020-02-20 13:25:37 +0000

    Minor modifications to the cmakelists and dependancies

--- BurstTrigger/SNBurstTrigger.hh
+++ BurstTrigger/SNBurstTrigger.hh
@@ -7,14 +7,14 @@
 class SNBurstTrigger {
 private:
   int PoissonStatThreshold;
-  bool reproduceAlexResult; 
-  bool faster;
+//  bool reproduceAlexResult; 
+  //bool faster;
 public:
   
   SNBurstTrigger():
-    PoissonStatThreshold(10),
-    reproduceAlexResult(false),
-    faster(true){};
+    PoissonStatThreshold(10){}
+    //  reproduceAlexResult(false),
+    //faster(true){};
   void SetPoissonStatThreshold(const int c) { PoissonStatThreshold = c; };
   void FillFakeRateNClusters(Configuration& c) const;
   void FindOnePerMonthFakeRate(Configuration& c, const double TargetRate=1./3600./24./31) const;
