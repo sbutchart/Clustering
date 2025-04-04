6ad0843b0b25378586b235f3763aab89e1450d6b
Sophie Butchart <sb2125@sussex.ac.uk>
2025-04-04 10:20:44 -0500

    Tweaks to TA triggering scripts

--- TriggerDecision/app/DumpTP.cxx
+++ TriggerDecision/app/DumpTP.cxx
@@ -10,7 +10,6 @@
 #include "TLeaf.h"
 
 #include "Utils/CLI11.hpp"
-#include "Utils/Helper.h"
 
 using namespace std;
 
@@ -20,28 +19,46 @@ int main(int argc, char** argv) {
   string InputChainName = "triggerAnaDumpAll/TriggerActivities/tamakerTPC__TriggerAnaTree1x2x6";
   string InputTrueChainName = "triggerAnaDumpAll/mctruths";
   string Feature = "adc_integral";
-  string InputFile = "";
+  string InputSignalFile = "";
+  string InputBackgroundFile = "";
   string OutputFileName = "LikelihoodTrigger.root";
   string type = "s";
 
-  app.add_option("-l,--list",  InputFile, "Input file list")->required();
+  app.add_option("-s,--signal",  InputSignalFile, "Input signal file list")->required();
+  app.add_option("-b,--background", InputBackgroundFile, "Input background file list")->required();
   app.add_option("-o,--output", OutputFileName, "Output file name");
-  app.add_option("-t,--type", type, "Signal (s) or Background (b)")->required();
+  //app.add_option("-t,--type", type, "Signal (s) or Background (b)")->required();
   CLI11_PARSE(app, argc, argv);
 
-  unique_ptr<TChain> InputChain     = make_unique<TChain>(InputChainName    .c_str());
-  unique_ptr<TChain> InputTrueChain = make_unique<TChain>(InputTrueChainName.c_str());
+  unique_ptr<TChain> InputSignalChain         = make_unique<TChain>(InputChainName    .c_str());
+  unique_ptr<TChain> InputTrueSignalChain     = make_unique<TChain>(InputTrueChainName.c_str());
+
+  unique_ptr<TChain> InputBackgroundChain     = make_unique<TChain>(InputChainName    .c_str());
+  unique_ptr<TChain> InputTrueBackgroundChain = make_unique<TChain>(InputTrueChainName.c_str());
   
-  string line;
-  ifstream myfile (InputFile);
-  if (myfile.is_open()){
+  string sline;
+  ifstream mysfile (InputSignalFile);
+  if (mysfile.is_open()){
     cout << "Parsing list of files...\n";
-    while ( getline (myfile,line) ){
-      InputChain->Add(line.c_str());
-      InputTrueChain->Add(line.c_str());
+    while ( getline (mysfile,sline) ){
+      InputSignalChain->Add(sline.c_str());
+      InputTrueSignalChain->Add(sline.c_str());
     }
   } else {
-    cerr << "File " << InputFile << " doesnt exist.\n";
+    cerr << "File " << InputSignalFile << " doesnt exist.\n";
+    throw;
+  }
+
+  string bline;
+  ifstream mybfile (InputBackgroundFile);
+  if (mybfile.is_open()){
+    cout << "Parsing list of files...\n";
+    while ( getline (mybfile,bline) ){
+      InputBackgroundChain->Add(bline.c_str());
+      InputTrueBackgroundChain->Add(bline.c_str());
+    }
+  } else {
+    cerr << "File " << InputBackgroundFile << " doesnt exist.\n";
     throw;
   }
 
@@ -49,54 +66,76 @@ int main(int argc, char** argv) {
   
   //map<int,shared_ptr<TH1D>> PDF_Background_notype;
   shared_ptr<TH1D> PDF_Background_notype;
-  PDF_Background_notype = make_shared<TH1D>("PDF_Background", "PDF;SADC;", 20, 0, 200);
+  PDF_Background_notype = make_shared<TH1D>("PDF_Background", "PDF;SADC;", 20, 0, 20000);
+
+  shared_ptr<TH1D> PDF_Signal;
+  PDF_Signal = make_shared<TH1D>("PDF_Signal", "PDF;SADC;", 20, 0, 20000);
 
   //map<TFile*,map<int,shared_ptr<TH1D>>> PDF_Background_perfile_notype;
 
-  double SumADC=0;
+  ULong_t SumADCS=0;
+  ULong_t SumADCB=0;
   int Event_true = 0;
   
-  InputChain->SetBranchAddress(Feature.c_str(), &SumADC);
+  //InputChain->SetBranchAddress(Feature.c_str(), &SumADC);
+  InputSignalChain->SetBranchAddress("adc_integral", &SumADCS);
+  InputBackgroundChain->SetBranchAddress("adc_integral", &SumADCB);
   //std::cout << "Set ADC integral branch" << std::endl;
 
   //InputTrueChain->SetBranchAddress("en",       &ENu       );
-  InputTrueChain->SetBranchAddress("Event",    &Event_true);
+  InputSignalChain->SetBranchAddress("Event",    &Event_true);
   //std::cout << "Set true event branch" << std::endl;
 
-  int nEventsGenerated=InputTrueChain->GetMaximum("Event");
-  std::cout << "nEventsGenerated: " << nEventsGenerated << std::endl;
+  int nEventsGeneratedS = InputTrueSignalChain->GetMaximum("Event");
+  std::cout << "Number of signal events: " << nEventsGeneratedS << std::endl;
 
+  int nEventsGeneratedB = InputTrueBackgroundChain->GetMaximum("Event");
+  std::cout << "Number of background events: " << nEventsGeneratedB << std::endl;
+
+  std::cout << "GetEntries: " << std::endl;
+  int nEntriesS = InputSignalChain->GetEntries("adc_integral");
+  std::cout << "nEntriesS: " << nEntriesS << std::endl;
+
+  int nEntriesB = InputBackgroundChain->GetEntries("adc_integral");
+  std::cout << "nEntriesB: " << nEntriesB << std::endl;
 
   std::cout << "Create and open output file:" << std::endl;
   unique_ptr<TFile> OutputFile = make_unique<TFile>(OutputFileName.c_str(), "RECREATE");
   OutputFile->cd();
 
-  std::cout << "GetMaximum: " << std::endl;
-  int nEntries = InputChain->GetMaximum("adc_integral");
+  //std::cout << "GetMaximum: " << std::endl;
+  //int nEntries = InputChain->GetEntries();
   //int nEntries = 500;
   
   std::cout << "GetEntry loop..." << std::endl;
-  for (int iEntry=0; iEntry<nEntries; ++iEntry) {
-    InputChain->GetEntry(iEntry);
-    PrintProgress(iEntry,nEntries);
+  for (int iEntry=0; iEntry<nEntriesS; ++iEntry) {
+    InputSignalChain->GetEntry(iEntry);
+    //PrintProgress(iEntry,nEntries);
+    PDF_Signal->Fill(SumADCS);
+   }
 
-    if (type == "s") {
-    } else {
-      PDF_Background_notype->Fill(SumADC/100.);
-       }
-    }
+  for (int iEntry=0; iEntry<nEntriesB; ++iEntry) {
+    InputBackgroundChain->GetEntry(iEntry);
+    PDF_Background_notype->Fill(SumADCB);
+  }
 
   OutputFile->cd();
 
   std::cout << "Scaling." << std::endl;
 
-  double scaleBackground = 1. / 2.2e-3 / nEventsGenerated / 0.12;
+  double scaleBackground = 1. / 2.2e-3 / nEventsGeneratedS / 0.12;
+  double scaleSignal = 1.;
 
-  if (type == "b") {
-    PDF_Background_notype->Scale(scaleBackground);
-    PDF_Background_notype->Write();
 
-  }
+  PDF_Background_notype->Fill(7000,1.);
+  PDF_Background_notype->Fill(8000,1.);
+  PDF_Background_notype->Fill(9000,1.);
+ 
+  PDF_Background_notype->Scale(scaleBackground);
+  PDF_Background_notype->Write();
+
+  PDF_Signal->Scale(scaleSignal);
+  PDF_Signal->Write();
 
       OutputFile->Close();
 }
---
+++ TriggerDecision/app/RunSmartTriggerStatisticalTestsTA.cxx
@@ -0,0 +1,70 @@
+//____________________________________________________________________ 
+//  
+// $Id$ 
+// Author: Pierre Lasorak <plasorak@Pierres-MacBook-Pro.local>
+// Update: 2019-02-14 15:38:26+0000
+// Copyright: 2019 (C) Pierre Lasorak
+//
+//
+
+#include "TriggerDecision/StatisticalTest.hh"
+#include "TriggerDecision/TriggerSensitivityCalculatorTA.hh"
+
+#include "Utils/Helper.h"
+#include "Utils/CLI11.hpp"
+
+#include "TCanvas.h"
+#include "TColor.h"
+#include "TFile.h"
+#include "TGraph.h"
+#include "TH1D.h"
+#include "TLegend.h"
+#include <TMath.h>
+#include "TPad.h"
+#include "TRandom3.h"
+#include "TText.h"
+#include "TVectorT.h"
+
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+
+int main(int argc, char** argv) {
+
+  CLI::App app{"SmartTrigger"};
+  std::string InputSignalFileName  = "";
+  std::string InputBackgroundFileName  = "";
+  std::string OutputFileName = "";
+  std::string Method = "Likelihood";
+  
+  int nToy, nThread=1, nEvent;
+  app.add_option("--ntoy",            nToy,                    "Number of toys to throw"          )->required();
+  app.add_option("--nthread",         nThread,                 "Number of threads to use"         );
+  app.add_option("--nevent",          nEvent,                  "Number of event in the SN"        )->required();
+  app.add_option("--output",          OutputFileName,          "Output file name (root)"          )->required();
+  app.add_option("--inputsignal",     InputSignalFileName,     "Input signal file name (root)"    )->required();
+  app.add_option("--inputbackground", InputBackgroundFileName, "Input background file name (root)")->required();
+  app.add_option("--method",          Method,                  "Method can be \"Likelihood\", \"LikelihoodNorm\" or \"LikelihoodShape\"")->required();
+  
+  CLI11_PARSE(app, argc, argv);
+
+  std::cout << "Start TSC" << std::endl;
+  TriggerSensitivityCalculator tsc(nEvent, 10, nToy, nThread,
+                                   InputSignalFileName,
+                                   InputBackgroundFileName,
+                                   OutputFileName,
+                                   Method);
+
+  std::cout << "Computing:" << std::endl;
+  tsc.Compute();
+  
+  return 0;
+}
+
+//____________________________________________________________________ 
+//  
+// EOF
+//
+//____________________________________________________________________ 
+//
